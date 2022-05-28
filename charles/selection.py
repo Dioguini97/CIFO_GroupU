2e615e29402cfeb39fc5808a05595588e23cb891
Maya Tagunova <maya.tagunova@gmail.com>
2022-05-28 18:48:52 +0100

    Implemented fps for minimization

--- charles/selection.py
+++ charles/selection.py
@@ -11,12 +11,12 @@ def fps(population):
     Returns:
         Individual: selected individual.
     """
+    # Sum total fitness
+    total_fitness = sum([i.fitness for i in population])
+    # Get a 'position' on the wheel
+    spin = uniform(0, total_fitness)
 
     if population.optim == "max":
-        # Sum total fitness
-        total_fitness = sum([i.fitness for i in population])
-        # Get a 'position' on the wheel
-        spin = uniform(0, total_fitness)
         position = 0
         # Find individual in the position of the spin
         for individual in population:
@@ -25,7 +25,12 @@ def fps(population):
                 return individual
 
     elif population.optim == "min":
-        raise NotImplementedError
+        position = total_fitness
+        # Find individual in the position of the spin
+        for individual in population:
+            position -= individual.fitness
+            if position < spin:
+                return individual
 
     else:
         raise Exception("No optimization specified (min or max).")

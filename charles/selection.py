b452fa9080b1893b5cd1fd202710dff5dd3637af
Guzel2021 <90806986+Guzel2021@users.noreply.github.com>
2022-05-29 00:44:39 +0100

    Added Ranking selection

--- charles/selection.py
+++ charles/selection.py
@@ -36,7 +36,7 @@ def fps(population):
         raise Exception("No optimization specified (min or max).")
 
 
-def tournament(population, size=10):
+def tournament(population, size=2):
     """Tournament selection implementation.
 
     Args:
@@ -56,4 +56,49 @@ def tournament(population, size=10):
         return min(tournament, key=attrgetter("fitness"))
     else:
         raise Exception("No optimization specified (min or max).")
+
+
+
+
+def ranking(population):
+    """Linear ranking selection implementation.
+
+    Args:
+        population (Population): The population we want to select from.
+
+    Returns:
+        Individual: selected individual.
+    """
+    # Get the number of individuals in the population.
+    n = population.size
+    # Get a 'position' on the wheel
+    spin = uniform(0, n)
+
+    # # Use the gauss formula to get the sum of all ranks (sum of integers 1 to N).
+    rank_sum = n * (n + 1) / 2
+
+    # Sort and go through all individual fitnesses; enumerate ranks from 1.
+    for rank, individual in enumerate(sorted(population), 1):
+        yield rank, individual.fitness, rank / rank_sum
+
+    if population.optim == "max":
+        position = 0
+        # Find individual in the position of the spin
+        for individual in population:
+            position += float(rank)
+            if position > spin:
+                return individual
+
+    elif population.optim == "min":
+        position = rank_sum
+        # Find individual in the position of the spin
+        for individual in population:
+            position -= float(rank)
+            if position < spin:
+                return individual
+
+    else:
+        raise Exception("No optimization specified (min or max).")
+
+
 

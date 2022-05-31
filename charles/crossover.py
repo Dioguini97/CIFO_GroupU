3ee9c3677aa2dfd207a12890000d6a92fe2ac1df
Maya Tagunova <maya.tagunova@gmail.com>
2022-05-31 15:05:21 +0100

    Implemented multi point crossover

--- charles/crossover.py
+++ charles/crossover.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, sample
 
 
 def single_point_co(p1, p2):
@@ -9,12 +9,32 @@ def single_point_co(p1, p2):
         p2 (Individual): Second parent for crossover.
 
     Returns:
-        Individuals: Two offspring, resulting from the crossover.
+        Individuals: Two offsprings, resulting from the crossover.
     """
     co_point = randint(1, len(p1)-2)
 
     offspring1 = p1[:co_point] + p2[co_point:]
     offspring2 = p2[:co_point] + p1[co_point:]
+
+    return offspring1, offspring2
+
+
+def multi_point_co(p1, p2):
+    """Implementation of single point crossover.
+
+    Args:
+        p1 (Individual): First parent for crossover.
+        p2 (Individual): Second parent for crossover.
+
+    Returns:
+        Individuals: Two offsprings, resulting from the crossover.
+    """
+
+    co_points = sample(range(1, len(p1)-2), 2)
+    co_points.sort()
+
+    offspring1 = p1[:co_points[0]] + p2[co_points[0]:co_points[1]] + p1[co_points[1]:]
+    offspring2 = p2[:co_points[0]] + p1[co_points[0]:co_points[1]] + p2[co_points[1]:]
 
     return offspring1, offspring2
 
@@ -27,7 +47,7 @@ def cycle_co(p1, p2):
         p2 (Individual): Second parent for crossover.
 
     Returns:
-        Individuals: Two offspring, resulting from the crossover.
+        Individuals: Two offsprings, resulting from the crossover.
     """
 
     # Offspring placeholders - None values make it easy to debug for errors
